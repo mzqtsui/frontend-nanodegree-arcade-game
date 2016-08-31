e38ab3609a9ad90b12789852078573b962ec7a74
Matthew Tsui <matthew.zq.tsui@gmail.com>
2016-08-31 13:56:40 -0400

    Enemy uses es6 class

--- js/app.js
+++ js/app.js
@@ -1,36 +1,38 @@
 const MAX_ENEMIES = 5;
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor(x, y, speed) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-};
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += dt*this.speed;
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        this.x += dt*this.speed;
 
-    return this.x;
-};
+        return this.x;
+    };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 
-Enemy.prototype.reset = function() {
-    this.x = -100;
+    reset() {
+        this.x = -100;
+    }
 }
 
 // Now write your own player class
